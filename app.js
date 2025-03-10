71ce1207d7042cca4005e4bbfaac30c273ae729c
Atindra Ghosh <atindraghosh2017@gmail.com>
2025-03-10 21:16:25 +0530

    changed index to app

---
+++ app.js
@@ -0,0 +1,5 @@
+import express from "express";
+import {config} from "dotenv";
+import cookieparser from "cookie-parser";
+
+export const index = express();
--- index.js
+++
@@ -1,3 +0,0 @@
-import express from "express";
-import {config} from "dotenv";
-import cookieparser from "cookie-parser";
