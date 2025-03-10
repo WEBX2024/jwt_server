660d21704082e935727b6796682cc64f66bdee3d
Atindra Ghosh <atindraghosh2017@gmail.com>
2025-03-10 22:04:08 +0530

    modified app.js

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 import express from "express";
-import "dotenv";
+import {config} from "dotenv";
 import cookieparser from "cookie-parser";
-
+import cors from cors;
 export const app = express(); 
+config({path:"./config.env"}) ;
+
+
+app.use(cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ["GET","POST","PUT","DELETE"],
+    Credentials: true,
+})) //for using middleware
+
+
+app.use(cookieparser()); // middle ware
+app.use(express.json()); //used for data parsing (middleware)
+app.use(express.urlencoded({extended: true })); //help us to understand which type of data is coming from frontend to backend
+
