1bad2ed388b1e8938934296fa3e0f32dd57af6b6
Atindra Ghosh <atindraghosh2017@gmail.com>
2025-03-10 21:30:27 +0530

    modified app.js and added server.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {config} from "dotenv";
+import "dotenv";
 import cookieparser from "cookie-parser";
 
-export const index = express();
+export const app = express();
